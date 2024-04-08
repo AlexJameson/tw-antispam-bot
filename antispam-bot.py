1c5f779353a449f18e8ce3c9abc2049ad0cccbbd
Alexander Yakovlev <33040934+AlexJameson@users.noreply.github.com>
2024-04-08 20:38:42 +0300

    Implement user ban

--- antispam-bot.py
+++ antispam-bot.py
@@ -32,14 +32,14 @@ async def report_manually(update: Update, context: CallbackContext):
         user_link = f"https://t.me/{user.username}"
         link = f"https://t.me/c/{chat_id}/{message_id}"
         keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Delete", callback_data=f"{chat_id}, {message_id}, {user.id}")]
+            [InlineKeyboardButton("Delete", callback_data=f"{chat_id}, {message_id}, {user.id}, {update.message.message_id}")]
         ])
 
         spam_indicator = 0
         if reply_to_message.text is not None:
             words = reply_to_message.text.split()
             message_text = reply_to_message.text_html_urled
-            text_message_content = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n<a href='{link}'>Go to chat</a>\n\n@{BACKUP_ADMIN}"
+            text_message_content = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n<a href='{link}'>Go to chat</a>\n\n@{PRIMARY_ADMIN} @{BACKUP_ADMIN}"
             await context.bot.send_message(chat_id=TARGET_CHAT,
                                     text=text_message_content,
                                     disable_web_page_preview=True,
@@ -48,7 +48,7 @@ async def report_manually(update: Update, context: CallbackContext):
         if reply_to_message.text is None:
             words = reply_to_message.caption.split()
             message_text = reply_to_message.caption_html_urled
-            new_caption = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n<a href='{link}'>Go to chat</a>\n\n@{BACKUP_ADMIN}"
+            new_caption = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n<a href='{link}'>Go to chat</a>\n\n@{PRIMARY_ADMIN} @{BACKUP_ADMIN}"
             await context.bot.copy_message(chat_id=TARGET_CHAT,
                                     from_chat_id=reply_to_message.chat_id,
                                     message_id=reply_to_message.message_id,
@@ -66,11 +66,13 @@ async def button_delete(update: Update, context: CallbackContext):
     await query.answer()
     callback_data = query.data.split(',')
     message_id = callback_data[1].strip()
+    command_id = callback_data[3].strip()
     chat_id_temp = str(callback_data[0])
     chat_id=f"-100{chat_id_temp}"
     await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+    await context.bot.delete_message(chat_id=chat_id, message_id=command_id)
     user_id = callback_data[2].strip()
-    #context.bot.ban_chat_member(chat_id=chat_id, user_id=user_id)
+    context.bot.ban_chat_member(chat_id=chat_id, user_id=user_id)
     user = query.from_user
     if user.last_name is not None:
        user_display_name = f"{user.first_name} {user.last_name}"
