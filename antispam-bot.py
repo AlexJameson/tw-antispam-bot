9e64a9670c1de791df6ad3e35751099b08043493
Alexander Yakovlev <33040934+AlexJameson@users.noreply.github.com>
2024-04-09 11:44:40 +0300

    implement basic scoring

--- antispam-bot.py
+++ antispam-bot.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import logging
 import os
+import re
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-
-from telegram.ext import ApplicationBuilder, CallbackContext, MessageHandler, filters, CommandHandler, CallbackQueryHandler
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, CallbackQueryHandler
+from spam_tokens import REGULAR_TOKENS, CRITICAL_TOKENS
 
 logging.basicConfig(level=logging.WARNING, 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
@@ -32,23 +33,32 @@ async def report_manually(update: Update, context: CallbackContext):
         user_link = f"https://t.me/{user.username}"
         link = f"https://t.me/c/{chat_id}/{message_id}"
         keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Delete", callback_data=f"{chat_id}, {message_id}, {user.id}, {update.message.message_id}")]
+            [InlineKeyboardButton("Удалить", callback_data=f"{chat_id}, {message_id}, {user.id}, {update.message.message_id}")]
         ])
-
-        spam_indicator = 0
         if reply_to_message.text is not None:
-            words = reply_to_message.text.split()
+            words = reply_to_message.text
+            
+        elif reply_to_message.text is None:
+            words = reply_to_message.caption
+        
+        regular_patterns = [pattern for pattern in REGULAR_TOKENS if re.search(pattern, words)]
+        num_regular = len(regular_patterns)
+        critical_patterns = [pattern for pattern in CRITICAL_TOKENS if re.search(pattern, words)]
+        num_critical = len(critical_patterns)
+        verdict = f"<b>Критические токены:</b> {num_regular}\n<b>Обычные токены:</b> {num_critical}"
+
+
+        if reply_to_message.text is not None:
             message_text = reply_to_message.text_html_urled
-            text_message_content = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n<a href='{link}'>Go to chat</a>\n\n@{PRIMARY_ADMIN} @{BACKUP_ADMIN}"
+            text_message_content = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n{verdict}\n\n<a href='{link}'>Открыть в чате</a>\n\n@{PRIMARY_ADMIN} @{BACKUP_ADMIN}"
             await context.bot.send_message(chat_id=TARGET_CHAT,
                                     text=text_message_content,
                                     disable_web_page_preview=True,
                                     parse_mode="HTML",
                                     reply_markup=keyboard)
-        if reply_to_message.text is None:
-            words = reply_to_message.caption.split()
+        elif reply_to_message.text is None:
             message_text = reply_to_message.caption_html_urled
-            new_caption = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n<a href='{link}'>Go to chat</a>\n\n@{PRIMARY_ADMIN} @{BACKUP_ADMIN}"
+            new_caption = f"<a href='{user_link}'><b>{user_display_name}</b></a>\n\n{message_text}\n\n{verdict}\n\n<a href='{link}'>Открыть в чате</a>\n\n@{PRIMARY_ADMIN} @{BACKUP_ADMIN}"
             await context.bot.copy_message(chat_id=TARGET_CHAT,
                                     from_chat_id=reply_to_message.chat_id,
                                     message_id=reply_to_message.message_id,
@@ -58,7 +68,7 @@ async def report_manually(update: Update, context: CallbackContext):
     else:
         await context.bot.send_message(
             chat_id=update.message.chat_id,
-            text="Reply to a message with the /ban command."
+            text="Ответьте на сообщение командой /ban."
         )
 
 async def button_delete(update: Update, context: CallbackContext):
@@ -72,14 +82,14 @@ async def button_delete(update: Update, context: CallbackContext):
     await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
     await context.bot.delete_message(chat_id=chat_id, message_id=command_id)
     user_id = callback_data[2].strip()
-    # await context.bot.ban_chat_member(chat_id=chat_id, user_id=user_id)
+    await context.bot.ban_chat_member(chat_id=chat_id, user_id=user_id)
     user = query.from_user
     if user.last_name is not None:
        user_display_name = f"{user.first_name} {user.last_name}"
     elif user.last_name is None:
        user_display_name = f"{user.first_name}"
     user_link = f"https://t.me/{user.username}"
-    await query.message.reply_html(f"<a href='{user_link}'><b>{user_display_name}</b></a> banned the user with the ID {user_id}", disable_web_page_preview=True)
+    await query.message.reply_html(f"<a href='{user_link}'><b>{user_display_name}</b></a> забанил пользователя с ID {user_id}", disable_web_page_preview=True)
 
 
 def main():
